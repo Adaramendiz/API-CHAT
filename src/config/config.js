18b21fd0efa0a2f9e40996c4bbd39cf855d1130b
andres <adaramendiz@gmail.com>
2023-10-10 17:01:01 -0500

    update

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,7 @@ module.exports = {
     database: "chat_db",
     host: "localhost",
     dialect: "postgres",
+    logging: false,
   },
   test: {
     username: "root",
@@ -16,10 +17,13 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false, 
+    dialectOptions: { ssl: { required: true, rejectUnauthorized: false } },
   },
 };
